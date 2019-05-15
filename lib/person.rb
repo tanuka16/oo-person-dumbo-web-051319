ec128d66b955af3a832d885c05762165dc7d1dd7
Tanuka16 <das.tanuka@yahoo.com>
2019-05-15 17:45:17 -0400

    pair_project with Otha

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,48 @@
-# your code goes here
+# your code goes here\
+require "pry"
+class Person
+  attr_accessor :bank_account, :happy, :hygiene
+  attr_reader :name
+
+
+  def initialize(name, bank_account= 25, happy, hygiene)
+    @name = name
+    @bank_account = bank_account
+    @happy = happy
+    @hygiene = hygiene
+
+  end
+  def rem
+    self.name =
+  end
+  # def bank_account(money)
+  #   @bank_account = money
+  # end
+
+  # def happy(points)
+  #   @happy = points
+  # end
+
+  # def hygiene(points)
+  #   @hygiene = points
+  # end
+
+  #setter method
+
+  def happy=(points)
+    if points <= 10
+      @happy = points
+    end
+  end
+
+  def hygiene=(points)
+    if points <= 10
+      @hygiene = points
+    end
+  end
+
+end
+
+
+
+binding.pry
